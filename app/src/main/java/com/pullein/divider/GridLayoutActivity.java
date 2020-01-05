17759bd37196f41bad50f35ae3fdcc0cd5522807
xugang <xugang2801779@126.com>
2020-01-05 20:48:04 +0800

    添加缩放动画

--- app/src/main/java/com/pullein/divider/GridLayoutActivity.java
+++ app/src/main/java/com/pullein/divider/GridLayoutActivity.java
@@ -2,6 +2,9 @@ package com.pullein.divider;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,5 +35,23 @@ public class GridLayoutActivity extends BaseActivity {
         recyclerView.addItemDecoration(new GridDecoration(Color.RED, Color.BLUE, 10, 20));
         recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         recyclerView.setAdapter(new GridAdapter(this));
+    }
+
+    public void show(View view) {
+        ScaleAnimation animation = new ScaleAnimation(0,1,0,1, Animation.RELATIVE_TO_PARENT,0.5f,1,0.5f);
+        animation.setDuration(100);
+        animation.setFillAfter(true);
+        animation.setRepeatCount(0);
+        recyclerView.setAnimation(animation);
+        recyclerView.setVisibility(View.VISIBLE);
+    }
+
+    public void hide(View view) {
+        ScaleAnimation animation = new ScaleAnimation(1,0,1,0, Animation.RELATIVE_TO_PARENT,0.5f,1,0.5f);
+        animation.setDuration(100);
+        animation.setFillAfter(true);
+        animation.setRepeatCount(0);
+        recyclerView.setAnimation(animation);
+        recyclerView.setVisibility(View.GONE);
     }
 }
