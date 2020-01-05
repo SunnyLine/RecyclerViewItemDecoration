ee6ae81744dbfe5f6f6143d0587e6fe91eac4468
xugang <xugang2801779@126.com>
2020-01-05 22:18:02 +0800

    调整UI，下一步解决最后一列宽度问题

--- app/src/main/java/com/pullein/divider/GridLayoutActivity.java
+++ app/src/main/java/com/pullein/divider/GridLayoutActivity.java
@@ -12,6 +12,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.pullein.divider.adapter.GridAdapter;
 import com.pullein.divider.decoration.GridDecoration;
 
+import static android.view.animation.Animation.RELATIVE_TO_SELF;
+
 /**
  * RecyclerView Item Decoration<br>
  * describe ：Recycle实现表格布局，行列间距
@@ -32,13 +34,13 @@ public class GridLayoutActivity extends BaseActivity {
     protected void initView(Bundle savedInstanceState) {
         initToolbar(R.string.gridlayout);
         recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.addItemDecoration(new GridDecoration(Color.RED, Color.BLUE, 10, 20));
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        recyclerView.addItemDecoration(new GridDecoration(Color.RED, Color.BLUE, 20, 35));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
         recyclerView.setAdapter(new GridAdapter(this));
     }
 
     public void show(View view) {
-        ScaleAnimation animation = new ScaleAnimation(0,1,0,1, Animation.RELATIVE_TO_PARENT,0.5f,1,0.5f);
+        ScaleAnimation animation = new ScaleAnimation(0,1,0,1, RELATIVE_TO_SELF,0.5f,RELATIVE_TO_SELF,0.5f);
         animation.setDuration(100);
         animation.setFillAfter(true);
         animation.setRepeatCount(0);
@@ -47,7 +49,7 @@ public class GridLayoutActivity extends BaseActivity {
     }
 
     public void hide(View view) {
-        ScaleAnimation animation = new ScaleAnimation(1,0,1,0, Animation.RELATIVE_TO_PARENT,0.5f,1,0.5f);
+        ScaleAnimation animation = new ScaleAnimation(1,0,1,0, RELATIVE_TO_SELF,0.5f,RELATIVE_TO_SELF,0.5f);
         animation.setDuration(100);
         animation.setFillAfter(true);
         animation.setRepeatCount(0);
